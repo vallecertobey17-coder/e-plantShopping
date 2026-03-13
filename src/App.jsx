68dd7ce8242c11410e2c40bfe923beaef5eb9312
vallecertobey17-coder <vallecertobey17@gmail.com>
2026-03-13 17:29:30 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-
 import React, { useState } from 'react';
 import ProductList from './ProductList';
 import './App.css';
 import AboutUs from './AboutUs';
 
 function App() {
-  
+  // State to toggle between the Landing Page and Product List
   const [showProductList, setShowProductList] = useState(false);
 
+  // Requirement: Logic for the "Get Started" button
   const handleGetStartedClick = () => {
     setShowProductList(true);
   };
@@ -18,24 +18,29 @@ function App() {
 
   return (
     <div className="app-container">
+      {/* Landing Page Section */}
       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
         <div className="background-image"></div>
         <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
+          <div className="landing_content">
+            {/* Requirement: Company Name */}
+            <h1>Welcome To Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Green Meets Serenity</p>
+            
+            {/* Requirement: Get Started Button */}
+            <button className="get-started-button" onClick={handleGetStartedClick}>
+              Get Started
+            </button>
+          </div>
+          
           <div className="aboutus_container">
-          <AboutUs/>
+            <AboutUs />
           </div>
-          </div>
-
+        </div>
       </div>
+
+      {/* Product List Section - displayed when showProductList is true */}
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
         <ProductList onHomeClick={handleHomeClick}/>
       </div>
